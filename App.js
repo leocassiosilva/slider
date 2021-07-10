1dd9f0cfa65d07b72614556373f6cebe18bf625a
leocassiosilva <leocassiosilva1234@gmail.com>
2021-07-09 23:06:44 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Slider from '@react-native-community/slider'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state ={
+      valor:50
+    };
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Slider
+          minimumValue={0}
+          maximumValue={100}
+          onValueChange={(valorSelecionado) => this.setState({valor:valorSelecionado})}
+          value={this.state.valor}
+          minimumTrackTintColor="#00FF00"
+          maximumTrackTintColor="#FF0000"
+        />
+
+        <Text style={{textAlign:'center', fontSize:30}}>
+          {this.state.valor.toFixed(0)}
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:50
   },
 });
+export default App;
